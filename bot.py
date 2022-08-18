9a555f72de1f3c9c39b9767d97b4925447bb6f8b
shiradzee <dessstro0@mail.ru>
2022-08-18 09:34:54 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,46 @@
+import telebot, wikipedia, re
+
+# Вводим токен нашего бота.
+
+bot = telebot.TeleBot('5746457091:AAER5Fx_AVUuwI7QdCEYfGxX3YDK_bNVWGQ')
+# Устанавливаем русский язык в Wikipedia
+wikipedia.set_lang("ru")
+# Чистим текст статьи в Wikipedia и ограничиваем его тысячей символов
+def getwiki(s):
+    try:
+        ny = wikipedia.page(s)
+        # Получаем первую тысячу символов
+        wikitext=ny.content[:1000]
+        # Разделяем по точкам
+        wikimas=wikitext.split('.')
+        # Отбрасываем все после последней точки
+        wikimas = wikimas[:-1]
+        # Создаем пустую переменную для текста
+        wikitext2 = ''
+        # Проходимся по строкам, где нет знаков «равно» (то есть все, кроме заголовков)
+        for x in wikimas:
+            if not('==' in x):
+                    # Если в строке осталось больше трех символов, добавляем ее к нашей переменной и возвращаем утерянные при разделении строк точки на место
+                if(len((x.strip()))>3):
+                   wikitext2=wikitext2+x+'.'
+            else:
+                break
+        # Теперь при помощи регулярных выражений убираем разметку
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\{[^\{\}]*\}', '', wikitext2)
+        # Возвращаем текстовую строку
+        return wikitext2
+    # Обработка запроса,если в вики нет такого слова.
+    except Exception as e:
+        return 'В энциклопедии нет информации об этом'
+# Обработка /start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Отправьте мне любое слово, и я найду его значение на Wikipedia')
+# Получение сообщений от пользователя
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, getwiki(message.text))
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
